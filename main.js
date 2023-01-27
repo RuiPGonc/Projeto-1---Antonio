fdf012d658f7ea9379355be41c813f39b8dbf67d
Rui Pedro Gonçalves <ruipgonc@gmail.com>
2023-01-27 12:42:01 +0000

    up

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-window.addEventListener('load', () => {
+/*window.addEventListener('load', () => {
 
   // * obtem o html do input
   const nomeInput = document.querySelector('#username');
@@ -24,26 +24,88 @@ window.addEventListener('load', () => {
 
 })
 
-function newElement() {
+*/
 
-  var li = document.createElement("li");
+
+function send(){
+  //e.preventDefault();
+  var input_user = document.getElementById("username").value;
+  var input_password = document.getElementById("password").value;
   
-  var inputValue = document.getElementById("descricaoAtividade").value;
- 
-  //createTextNode é usado para fornecer texto a um elemento
-  var t = document.createTextNode(inputValue);
+  if(input_user!="" && input_password!=""){
+  // * Grava em loca storage - browser
+    localStorage.setItem("nome",input_user);
+    //mudar de página
+    window.open("homePage.html","_self");
 
-  //AppendChild acrescenta ao elemento declarado o que for mencionado nos ().
-  li.appendChild(t);
+   
+    //se ao abrir o programa não existir uma lista de tarefas é criada uma.
+    if(localStorage.getItem("listaTarefas")===null){
+localStorage.setItem("listaTarefas",JSON.stringify([]))}
 
-  console.log(li);
-  if (inputValue == '') {
-     alert("Por favor escreva alguma coisa");
-  } else {
-    //Adiciona a nova atividade às já existentes
-    document.getElementById("listaAtividades").appendChild(li);
+  }else{
+    document.getElementById("msgAlerta").style.display="block"; //torna a mensagemd e erro visivel
   }
-  //faz reset ao campo 
-  document.getElementById("descricaoAtividade").value = "";
+}
+
+function irAdicionarTarefa(){
+  //e.preventDefault();
+  window.open("newTask.html","_self");
+}
+function irHomePage(){
+  window.open("homePage.html","_self");
+}
+
+function validarFormularioNovaTarefa(){
+var descr_input = document.getElementById("descricao").value;
+var nome_input= document.getElementById("nameTask").value;
+
+
+if(descr_input !="" && nome_input!=""){
+//criar um novo objeto "Tarefa"
+  let novaTarefa = new Object();
+  novaTarefa.nome=nome_input;
+  novaTarefa.descricao=descr_input;
+  novaTarefa.realizada=false;
+
+
+  //traz a lista de tarefas que já existe
+  let listaTarefas=JSON.parse(localStorage.getItem("listaTarefas"));
+  //adiciona a nova tarefa à lista que existe
+  listaTarefas.push(novaTarefa);
+ // * Grava em loca storage - browser
+ localStorage.setItem("listaTarefas",JSON.stringify(listaTarefas));
+ 
+ irHomePage();
+}else{
+  document.getElementById("mensagemErro").style.display="block";
+}
+  
 
 }
+
+window.addEventListener('load', (event) => {
+// obter o user from localstorage - browser
+  const utilizador = localStorage.getItem('nome');
+
+  //console.log(utilizador.value); // para imprimir na consola
+  var userLogado=document.getElementById("nomeLogado");
+  if (userLogado)
+    userLogado.innerHTML='Bem-Vindo!\n'+utilizador;
+ 
+
+    listaTarefas.forEach(function(task){
+    // vou aqui: Rui
+      console.log(task);
+    
+    });
+    
+    /*<input type="checkbox" id="tarefa1" name="tarefa1" value="Bike">
+     <label for="tarefa1"> testetetete</label><br>
+*/
+
+})
+
+
+
+
